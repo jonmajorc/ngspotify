9c45ab2244457c09d5ef83db510d0821ec9c7342
sirJconny <jmajorcondon@gmail.com>
2016-07-06 21:19:45 -0500

    Added the router and it works! ...Finally

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './components/navbar-top/navbar-top.component';
+import { AboutComponent } from './components/about/about.component';
+import { SearchComponent } from './components/search/search.component';
+import { ROUTER_DIRECTIVES } from '@angular/router';
 
 @Component({
     moduleId:module.id,
     selector: 'my-app',
     templateUrl: 'app.component.html',
-    directives:[NavbarComponent]
+    directives:[ROUTER_DIRECTIVES,NavbarComponent],
+    precompile:[AboutComponent,SearchComponent]
 })
-export class AppComponent { }
+export class AppComponent { 
+    constructor(){}
+}
