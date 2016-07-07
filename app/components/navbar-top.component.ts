7b1506542d7c596114b23b55a8445deede7e2e3f
sirJconny <jmajorcondon@gmail.com>
2016-07-06 19:45:26 -0500

    navbar component added

---
+++ app/components/navbar-top.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId:module.id,
+    selector: 'navbar-top',
+    templateUrl:'navbar-top.component.html'
+})
+export class NavbarComponent { }
